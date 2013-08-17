a6cffef77750bd08cbb9be90587881f000327d04
Hariadi Hinta <diperakui@yahoo.com>
2013-08-17 14:44:24 +0800

    Initial {{slugify}} helper

---
+++ slugify.js
@@ -0,0 +1,13 @@
+module.exports.register = register = function(Handlebars, options) {
+  
+  /**
+   * Convert strings into URL slugs.
+   * @example:
+   *   {{slugify "test test"}}
+   */
+  Handlebars.registerHelper('slugify', function (input) {
+  if (!input || typeof input !== 'string') return input;
+  return input.replace(/^\s+|\s+$/g, '').toLowerCase().replace(/[^a-z0-9 -]/g, '').replace(/\s+/g, '-').replace(/-+/g, '-');
+  });
+
+};
